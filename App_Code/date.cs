10ad601965bfd383ee676e5161ec1e520a468b00
Sachchidanand0 <101634683+Sachchidanand0@users.noreply.github.com>
2023-08-05 23:01:10 +0530

    Add files via upload

---
+++ App_Code/date.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for date
+/// </summary>
+public class date
+{
+
+    public static String convertDate(String dt)
+    {
+        String da = "";
+        String d = dt.Substring(0, 2);
+        String m = dt.Substring(3, 2);
+        String y = dt.Substring(6, 4);
+
+        da = m+"/"+d+"/" + y;
+        return da;
+
+    }
+    public date()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
