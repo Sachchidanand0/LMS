d26be87f3390f650faf9effbb425e34b32001cb6
Sachchidanand0 <101634683+Sachchidanand0@users.noreply.github.com>
2023-08-05 23:30:52 +0530

    Add files via upload

---
+++ LibraryManagement/App_Code/top.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for top
+/// </summary>
+public class top
+{
+    public static String conv_date(String dat)
+    {
+        String da = "";
+        String d = dat.Substring(0, 2);
+        String m = dat.Substring(3, 2);
+        String y = dat.Substring(6, 4);
+        da = m + "/" + d + "/" + y;
+        return da;
+
+    }
+}
